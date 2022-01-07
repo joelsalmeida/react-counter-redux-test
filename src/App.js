473b47ac2900d9a846c2f6c6217526dda2469314
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-01-07 18:49:04 -0300

    Passa "Provider" de App.js para Index.js. Cria primeiro teste utilizando renderWithRedux

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { Provider } from 'react-redux';
 import store from './store';
 import ButtonClicks from './ButtonClicks';
 import NumberClicks from './NumberClicks';
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Provider store={store}>
           <ButtonClicks />
           <NumberClicks />
-        </Provider>
       </div>
     );
   }
