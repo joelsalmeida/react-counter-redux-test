a3eb450d0d864feac48c2a6cd2a1270c4116a929
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-01-08 14:45:56 -0300

    Test: Testa se ao clicar  no botao, o numero de cliques aumenta

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { render, cleanup, screen } from '@testing-library/react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, cleanup, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 import { createStore, combineReducers } from 'redux';
 import clickReducer from './reducers';
 
 const renderWithRedux = (
   component,
-  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+  {
+    initialState,
+    store = createStore(combineReducers({ clickReducer }), initialState),
+  } = {}
 ) => {
   return {
     ...render(<Provider store={store}>{component}</Provider>),
     store,
-  }
-}
+  };
+};
 
 describe('testing clicks', () => {
   beforeEach(cleanup);
@@ -27,9 +30,17 @@ describe('testing clicks', () => {
   });
 
   test('the number of clicks must be 5', () => {
-    renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 }}});
-  
+    renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 } } });
+
     expect(screen.getByText('5')).toBeInTheDocument();
   });
-});
 
+  test('a click in a button should increment the value of clicks', () => {
+    renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 } } });
+
+    const buttonAdicionar = screen.queryByText('Clique aqui');
+    fireEvent.click(buttonAdicionar);
+
+    expect(screen.getByText('6')).toBeInTheDocument();
+  });
+});
